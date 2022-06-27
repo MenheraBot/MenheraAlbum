a6f6aa2285dfe1199c98f9d747e89182e7362343
Yslan Ramos <yslanramos.r@gmail.com>
2022-06-27 14:00:21 -0300

    feat: reading folder to list categories

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use actix_web::{
-    get, web,
-    App, HttpServer, Result, Responder
-};
-use serde::Serialize;
-use actix_files::Files;
-use std::time::Instant;
+use actix_web::{get, web, App, HttpServer, Responder, Result};
 
+use actix_files::Files;
+use serde::Serialize;
+use std::collections::HashMap;
+use std::fs;
+use std::time::Instant;
 
 #[derive(Serialize)]
 struct Ping {
@@ -25,73 +24,35 @@ impl Uptime {
     }
 }
 
-#[derive(Serialize)]
-struct AvailableAssets {
-    angry: u8,
-    bicuda: u8,
-    bite: u8,
-    cheek: u8,
-    cry: u8,
-    disgusted: u8,
-    fear: u8,
-    fodase: u8,
-    grumble: u8,
-    hug: u8,
-    humor: u8,
-    kill: u8,
-    kiss: u8,
-    laugh: u8,
-    mamar: u8,
-    pat: u8,
-    poke: u8,
-    punch: u8,
-    resurrect: u8,
-    sarrar: u8,
-    sarrar_sozinho: u8,
-    shot: u8,
-    shy: u8,
-    slap: u8,
-    sniff: u8,
-    think: u8,
+fn get_categories() -> HashMap<String, usize> {
+    let mut assets: HashMap<String, usize> = HashMap::new();
+    let paths = fs::read_dir("assets/").unwrap();
+
+    for dir in paths {
+        let path = dir.unwrap().path();
+        if path.is_dir() {
+            let dir_name = (&path.file_name()).unwrap().to_str().unwrap();
+
+            let images = fs::read_dir(&path.to_str().unwrap()).unwrap();
+            let count = images.count();
+
+            assets.insert(dir_name.to_string(), count);
+        }
+    }
+
+    return assets;
 }
 
 #[get("/")]
 async fn index() -> Result<impl Responder> {
-    let obj = AvailableAssets {
-        angry: 37,
-        bicuda: 31,
-        bite: 33,
-        cheek: 22,
-        cry: 26,
-        disgusted: 17,
-        fear: 21,
-        fodase: 37,
-        grumble: 18,
-        hug: 26,
-        humor: 50,
-        kill: 23,
-        kiss: 20,
-        laugh: 23,
-        mamar: 23,
-        pat: 26,
-        poke: 24,
-        punch: 22,
-        resurrect: 6,
-        sarrar: 5,
-        sarrar_sozinho: 2,
-        shot: 14,
-        shy: 39,
-        slap: 29,
-        sniff: 20,
-        think: 29,
-    };
-    Ok(web::Json(obj))
+   let categories = get_categories();
+    Ok(web::Json(categories))
 }
 
 #[get("/ping")]
 async fn ping(data: web::Data<Uptime>) -> Result<impl Responder> {
     let obj = Ping {
-        uptime: data.start_at.elapsed().as_millis()
+        uptime: data.start_at.elapsed().as_millis(),
     };
 
     Ok(web::Json(obj))
